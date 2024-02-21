0fcdf1a1f17855f1b729adf53d6148bdf8f2f832
thomasgsquinaia <thomasquinaia@gmail.com>
2024-02-21 15:45:17 -0300

    [thomasquinaia] fix: server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const {App}=require('./app')
 const {InitDatabase}=require('./config/sequelize')
 async function main(){
-    const db=await InitDatabase()
+    const db = await InitDatabase()
     const app = new App();
     app.init()
 }
